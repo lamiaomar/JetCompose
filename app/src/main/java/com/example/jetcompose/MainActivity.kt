d432384c53bb33853fc85d7c48e1f4343eb17d8c
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2022-01-24 22:58:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jetcompose/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.jetcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetcompose.ui.theme.JetComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetComposeTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(names: List<String> = listOf("World", "Compose", "Lamia", "Omar")) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value },
+            ) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun DefaultPreview() {
+    JetComposeTheme {
+        MyApp()
+    }
+}
